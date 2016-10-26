4e86370e5d535183d1eb297906cdd5ef2acaf6fa
Matt Smith <msmith@apaxsoftware.com>
2016-10-26 11:37:50 -0400

    Initial setup of ui-router

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -1,12 +1,21 @@
 import angular from 'angular';
-
+import uiRouter from 'angular-ui-router';
 import ScreenshotsModule from '../screenshots/screenshots.module';
 
 import appComponent from './app.component';
 
 const AppModule = angular.module('app', [
+    uiRouter,
     ScreenshotsModule.name,
 ])
-    .component('app', appComponent);
+    .component('app', appComponent)
+    .config(($stateProvider, $urlRouterProvider) => {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider.state('index', {
+            url: '/',
+            component: 'screenshotsList',
+        });
+    });
 
 export default AppModule;
