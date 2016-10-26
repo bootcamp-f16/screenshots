8188ab3c16da52317ff69f1d1dfd7d55bf9f7dcf
Matt Smith <msmith@apaxsoftware.com>
2016-10-26 07:32:34 -0400

    Add s3 support for media files on production.

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -1,3 +1,5 @@
+import os
+
 from .base import *
 
 import dj_database_url
@@ -8,3 +10,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_DEFAULT_REGION = 'us-east-1'
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
