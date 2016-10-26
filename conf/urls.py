52586ee7adc1f267cf1d7dd7ca20de8c05a85cf4
Matt Smith <msmith@apaxsoftware.com>
2016-10-26 06:41:26 -0400

    Serve media files locally.

--- conf/urls.py
+++ conf/urls.py
@@ -14,7 +14,15 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('core.urls', namespace='core')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
