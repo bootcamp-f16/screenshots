e307bd9563ed0190fa3a17c31e2208a859f94c60
Matt Smith <msmith@apaxsoftware.com>
2016-10-26 06:38:30 -0400

    Screenshot model.

--- screenshots/models.py
+++ screenshots/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Screenshot(models.Model):
+    screenshot = models.ImageField(
+        upload_to="screenshots/",
+        blank=True,
+        null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
