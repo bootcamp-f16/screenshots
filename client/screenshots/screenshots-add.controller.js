33581c079c60fba23181eea4f72ecc34b7f99b73
Matt Smith <msmith@apaxsoftware.com>
2016-10-27 11:21:49 -0400

    Wire up upload. Note: It does not work yet, gives a 403 forbidden error.

--- client/screenshots/screenshots-add.controller.js
+++ client/screenshots/screenshots-add.controller.js
@@ -1,9 +1,16 @@
 
-function ScreenshotsAddController() {
+function ScreenshotsAddController(screenshotsService) {
     const ctrl = this;
 
     ctrl.cancelUpload = function cancelUpload() {
         ctrl.file = null;
+    };
+
+    ctrl.uploadFile = function uploadFile() {
+        screenshotsService.uploadScreenshot(ctrl.file)
+            .then(() => {
+                alert('file uploaded');
+            });
     };
 }
 
