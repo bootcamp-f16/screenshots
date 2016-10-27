f9ec76e87d10701a49f5d0eabe8b2398cb32d673
Matt Smith <msmith@apaxsoftware.com>
2016-10-27 13:43:43 -0400

    Change the ordering on screenshots back from the API.

--- screenshots/viewsets.py
+++ screenshots/viewsets.py
@@ -4,5 +4,5 @@ from .models import Screenshot
 from .serializers import ScreenshotSerializer
 
 class ScreenshotViewSet(viewsets.ModelViewSet):
-    queryset = Screenshot.objects.all()
+    queryset = Screenshot.objects.all().order_by('-created_date')
     serializer_class = ScreenshotSerializer
