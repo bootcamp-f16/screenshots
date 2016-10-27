c72c593fcab3bbd22f5862a6f7143dd329fa9ef6
Matt Smith <msmith@apaxsoftware.com>
2016-10-27 17:10:46 -0400

    Redirect user to login if they try to go to screeshot angular application without loggin in first.

--- screenshots/urls.py
+++ screenshots/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.AppView.as_view(), name='app'),
+    url(r'^$', login_required(views.AppView.as_view()), name='app'),
 ]
