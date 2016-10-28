c15672acf6a0d9f0aa96c47d0e83e081d4d6ed8d
Matt Smith <msmith@apaxsoftware.com>
2016-10-28 14:28:41 -0400

    Restrict API so that only owners of screenshots can edit or delete a screenshot.

--- screenshots/views.py
+++ screenshots/views.py
@@ -1,14 +1,17 @@
 from django.views.generic import TemplateView
 from rest_framework import generics
 from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Screenshot
 from .serializers import ScreenshotUploadSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class ScreenshotUpload(generics.CreateAPIView):
     model = Screenshot
     serializer_class = ScreenshotUploadSerializer
     parser_classes = (MultiPartParser,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, )
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- screenshots/viewsets.py
+++ screenshots/viewsets.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Screenshot
 from .serializers import ScreenshotSerializer
+from .permissions import IsOwnerOrReadOnly
 
 class ScreenshotViewSet(viewsets.ModelViewSet):
     queryset = Screenshot.objects.all().order_by('-created_date')
     serializer_class = ScreenshotSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, )
