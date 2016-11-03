1bb23d6736bfc77cfe8d43a09ec7bbb131cd674d
Matt Smith <msmith@apaxsoftware.com>
2016-11-03 14:02:20 -0400

    Send email to all users that have an email address (will add each user to the 'to' field on the email.

--- screenshots/helpers.py
+++ screenshots/helpers.py
@@ -1,10 +1,19 @@
+from django.contrib.auth.models import User
 from django.core.mail import send_mail
 
 def send_new_screenshot_email(screenshot):
+    # Get all users that have an email address
+    user_emails = (User.objects
+        .exclude(email__isnull=True)
+        .values_list('email', flat=True))
+
+    # Get unique emails addresses
+    user_emails = list(set(user_emails))
+
     send_mail(
-        'New Screenshot',
+        'New Screenshot from Screenshots',
         'Test Message.',
         'matt@apaxsoftware.com',
-        ['mtsmit2@gmail.com'],
+        user_emails,
         fail_silently=False,
     )
