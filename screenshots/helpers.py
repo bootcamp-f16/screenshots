4e4b1b5cd5f54a185525a48706a539e55ad59d02
Matt Smith <msmith@apaxsoftware.com>
2016-11-04 09:23:28 -0400

    Use templates to render text and HTML version of emails.

--- screenshots/helpers.py
+++ screenshots/helpers.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.template.loader import get_template
 
 def send_new_screenshot_email(screenshot):
     # Get all users that have an email address
@@ -10,10 +12,14 @@ def send_new_screenshot_email(screenshot):
     # Get unique emails addresses
     user_emails = list(set(user_emails))
 
-    send_mail(
-        'New Screenshot from Screenshots',
-        'Test Message.',
-        'matt@apaxsoftware.com',
-        user_emails,
-        fail_silently=False,
-    )
+    # Get a text template to render
+    text_template = get_template('screenshots/emails/new_screenshot.txt')
+    html_template = get_template('screenshots/emails/new_screenshot.html')
+
+    subject, from_email = 'New Screenshot', settings.DEFAULT_FROM_EMAIL
+
+    text_content = text_template.render()
+    html_content = html_template.render()
+    msg = EmailMultiAlternatives(subject, text_content, from_email, user_emails)
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
